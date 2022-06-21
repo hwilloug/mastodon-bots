e88be55fc9f9391a88e3c554962601077d41e687
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-20 20:53:19 -0400

    Test

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+resource aws_aim_role iam_for_lambda {
+    name = "iam_for_lambda"
+
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+    ]
+}
+    EOF
+}
+
+resource aws_lambda_function test_lambda {
+    function_name = "lambda_function_name"
+    role          = aws_aim_role.iam_for_lambda.arn
+    
+    environment {
+        variables = {
+            foo = "bar"
+        }
+    }
+}
