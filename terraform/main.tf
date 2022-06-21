9e4a913978e1a18ff347910b0158005ef5a84934
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-20 22:08:13 -0400

    Fix typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_aim_role" "iam_for_lambda" {
+resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
@@ -34,7 +34,7 @@ resource "aws_aim_role" "iam_for_lambda" {
 
 resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
-  role          = aws_aim_role.iam_for_lambda.arn
+  role          = aws_iam_role.iam_for_lambda.arn
 
   environment {
     variables = {
