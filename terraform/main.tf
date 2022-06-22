65680bf3ca722115c406284129faf40ddd011695
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-21 23:45:31 -0400

    Add intial working pass at lambda terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,22 +19,36 @@ resource "aws_iam_role" "iam_for_lambda" {
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
-    EOF
+EOF
+}
+
+data "archive_file" "python_lambda_pachage" {
+  type        = "zip"
+  source_file = "../bots/gpu_bot/lambda_function.py"
+  output_path = "nametest.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+  filename      = "nametest.zip"
+
+  runtime = "python3.9"
+
+  timeout = 10
 
   environment {
     variables = {
