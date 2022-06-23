86013731aca4ddf1cdd8969c39261aa85190e544
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-22 22:28:18 -0400

    Fix reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_lambda_function" "gpu_bot_lambda" {
   function_name    = "gpu_bot_lambda"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
-  filename         = gpu_bot_lambda_package.output_path
+  filename         = archive_file.gpu_bot_lambda_package.output_path
   source_code_hash = filebase64sha256("lambda_pkg.zip")
 
   runtime = "python3.9"
