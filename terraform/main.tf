2b8ea48e83a3da5f3f32b18864adcde27832fdee
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-22 22:21:15 -0400

    Format tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,10 +46,10 @@ data "archive_file" "gpu_bot_lambda_package" {
 }
 
 resource "aws_lambda_function" "gpu_bot_lambda" {
-  function_name = "gpu_bot_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function.lambda_handler"
-  filename      = "lambda-pkg.zip"
+  function_name    = "gpu_bot_lambda"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  filename         = "lambda-pkg.zip"
   source_code_hash = filebase64sha256("lambda-pkg.zip")
 
   runtime = "python3.9"
