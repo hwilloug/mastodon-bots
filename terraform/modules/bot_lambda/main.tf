a1a5e36be82cd511fc90db918a5221bd592ee910
Hannah Willoughby <hannah@hannahwilloughby.dev>
2022-06-26 18:45:24 -0400

    Fix stuff

--- terraform/modules/bot_lambda/main.tf
+++ terraform/modules/bot_lambda/main.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
 }
 
 resource "aws_iam_role" "terraform_function_role" {
-  name               = "terraform_function_role"
+  name               = "${var.function_name}_terraform_function_role"
   assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
 }
 
@@ -19,7 +19,7 @@ resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-data "archive_file" "gpu_bot_lambda_package" {
+data "archive_file" "lambda_package" {
   type        = "zip"
   source_file = var.source_file
   output_path = "${var.function_name}-lambda.zip"
@@ -28,9 +28,9 @@ data "archive_file" "gpu_bot_lambda_package" {
 resource "aws_lambda_function" "gpu_bot_lambda" {
   function_name    = var.function_name
   role             = aws_iam_role.terraform_function_role.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "main.run"
   filename         = "${var.function_name}-lambda.zip"
-  source_code_hash = data.archive_file.gpu_bot_lambda_package.output_base64sha256
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
 
   runtime = "python3.9"
 
